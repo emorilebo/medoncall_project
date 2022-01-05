7a118031df9fda50b426e99e48f4d218fa2a6f60
emorilebo <emorylebo@gmail.com>
2022-01-05 23:57:06 +0100

    ADD Header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <meta name="description" content="MedOnCall" />
         <link rel="icon" href="/favicon.svg" />
+        <Header />
       </Head>
     </div>
   );
