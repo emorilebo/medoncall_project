fe5f41e8bd147408dbe20bf5bc36573854568619
emorilebo <emorylebo@gmail.com>
2022-01-06 00:05:49 +0100

    ADD Header Text

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,14 @@ function Header() {
   return (
     <div>
       {/* Left header */}
+      <div>
+        <div className="text-red-500">MedonCall</div>
+        <div>
+          <img src="ellipse.svg" alt="" />
+          <img src="ellipse.svg" alt="" />
+          <img src="ellipse.svg" alt="" />
+        </div>
+      </div>
       {/* Right Header */}
     </div>
   );
